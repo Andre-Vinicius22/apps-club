1a934be47c093b38daf91d4c0d03885ba7ca2ca3
Andre Vinicius <andre@bresults.com.br>
2022-11-28 12:52:53 -0300

    create list of packages

---
+++ src/components/packages/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './package.css'
+
+
+const Pacotes = () => {
+
+    const [count, setCount] = useState([]);
+    const [pacoteAtual, setpacoteAtual] = useState();
+
+    useEffect(() => {
+        const apiRequest = async () => {
+            try {
+                const response = await fetch("http://localhost:3000/pacote"); 
+                const lista = await response.json()
+                setCount(lista); 
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        apiRequest()
+      },[]);
+
+  return (
+      <div className="pacotes">
+        {count.map((item,index) => (
+            <button key={index} onClick={() =>  { setpacoteAtual(item.content)}}>{item.name}</button>
+        ))}
+        <div className='list'>
+            {pacoteAtual && pacoteAtual.map((item,index) => (
+                <div className='list-name' key={index}>{item.nameContent}</div>
+            ))}
+        </div>
+
+      </div>
+  );
+}
+
+export default Pacotes;
---
+++ src/components/title/index.js
@@ -0,0 +1,9 @@
+const Title = () => {
+    return(
+        <div className="title">
+            <span>Pacotes disponiveis</span>
+        </div>
+    )
+}
+
+export default Title;
