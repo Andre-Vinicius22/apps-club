f92c3910beaf09fc7ff8c49eb764bc438ae955c5
Andre Vinicius <89954543+Andre-Vinicius22@users.noreply.github.com>
2022-12-06 14:03:57 -0300

    changes on Mock

--- src/components/packages/index.js
+++ src/components/packages/index.js
@@ -10,29 +10,29 @@ const Pacotes = () => {
     useEffect(() => {
         const apiRequest = async () => {
             try {
-                const response = await fetch("http://localhost:3000/pacote"); 
+                const response = await fetch("http://localhost:3000/pacote");
                 const lista = await response.json()
-                setCount(lista); 
+                setCount(lista);
             } catch (error) {
                 console.log(error)
             }
         }
         apiRequest()
-      },[]);
+    }, []);
 
-  return (
-      <div className="pacotes">
-        {count.map((item,index) => (
-            <button key={index} onClick={() =>  { setpacoteAtual(item.content)}}>{item.name}</button>
-        ))}
-        <div className='list'>
-            {pacoteAtual && pacoteAtual.map((item,index) => (
-                <div className='list-name' key={index}>{item.nameContent}</div>
+    return (
+        <div className="pacotes">
+            {count.map((item, index) => (
+                <button key={index} onClick={() => { setpacoteAtual(item.content) }}>{item.name}</button>
             ))}
-        </div>
+            <div className='list'>
+                {pacoteAtual && pacoteAtual.map((item, index) => (
+                    <div className='list-name' key={index}>{item.nameContent}</div>
+                ))}
+            </div>
 
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Pacotes;
